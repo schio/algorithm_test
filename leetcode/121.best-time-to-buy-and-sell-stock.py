508863905fe35798b7621b676a8b3cddda7371aa
schio <ghsehr1@gmail.com>
2020-09-25 23:17:56 +0900

    Create 121.best-time-to-buy-and-sell-stock.py
    Accepted
    200/200 cases passed (60 ms)
    Your runtime beats 89.19 % of python3 submissions
    Your memory usage beats 93.31 % of python3 submissions (14.9 MB)

---
+++ leetcode/121.best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=121 lang=python3
+#
+# [121] Best Time to Buy and Sell Stock
+#
+
+# @lc code=start
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        min_price = sys.maxsize
+
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = max(profit, price - min_price)
+            
+        return profit
+        
+# @lc code=end
+
