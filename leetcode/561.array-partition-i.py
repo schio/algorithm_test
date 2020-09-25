74229d1870d70231648a65f27c5bdc334025c5d6
schio <ghsehr1@gmail.com>
2020-09-25 22:58:48 +0900

    Create 561.array-partition-i.py

---
+++ leetcode/561.array-partition-i.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=561 lang=python3
+#
+# [561] Array Partition I
+#
+
+# @lc code=start
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        return sum(sorted(nums)[::2])
+        
+# @lc code=end
+
