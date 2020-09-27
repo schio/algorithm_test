3df8910e807c86e9da11a2565a6a33f1c6f6655b
schio <ghsehr1@gmail.com>
2020-09-28 00:53:07 +0900

    Update 328.odd-even-linked-list.py
    Accepted
    71/71 cases passed (32 ms)
    Your runtime beats 99.46 % of python3 submissions
    Your memory usage beats 9.79 % of python3 submissions (15.9 MB)

--- leetcode/328.odd-even-linked-list.py
+++ leetcode/328.odd-even-linked-list.py
@@ -17,9 +17,11 @@ class Solution:
         odd = head
         even = head.next
         even_head = head.next
+
         while even and even.next:
             odd.next, even.next = odd.next.next, even.next.next
             odd, even = odd.next, even.next
+
         odd.next = even_head
         return head
         
