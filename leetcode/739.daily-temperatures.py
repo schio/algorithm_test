af82200c2318a3031cfd8054d167be4e861288b0
schio <ghsehr1@gmail.com>
2020-10-02 00:14:01 +0900

    Update 739.daily-temperatures.py
    error fix

--- leetcode/739.daily-temperatures.py
+++ leetcode/739.daily-temperatures.py
@@ -10,9 +10,14 @@ class Solution:
         answer = [0] * len(T)
         stack = []
         for i, cur in enumerate(T):
+
+            
+            # 이전보다 상승하는 지점에서 현재 온도와 스택에 쌓아둔 온도차 비교
             while stack and cur > T[stack[-1]]:
+                # 계속 온도가 떨어지며 처리되지 못한 것들을 pop
                 last = stack.pop()
-                a[last] = i - last
+                answer[last] = i - last
+            # 현재 인덱스를 스택에 쌓아 둠
             stack.append(i)
         return answer
         
