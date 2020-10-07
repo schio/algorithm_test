aadff3e2b62b94086107e9f7047b970dec1a2ad8
schio <ghsehr1@gmail.com>
2020-10-08 01:53:06 +0900

    Create 200.number-of-islands.py
    Accepted
    47/47 cases passed (140 ms)
    Your runtime beats 84.19 % of python3 submissions
    Your memory usage beats 21.53 % of python3 submissions (15.4 MB)

---
+++ leetcode/200.number-of-islands.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=200 lang=python3
+#
+# [200] Number of Islands
+#
+
+# @lc code=start
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != "1":
+                return
+            grid[i][j] = 0
+
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
+        cnt = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    cnt += 1
+        return cnt
+
+
+# @lc code=end
