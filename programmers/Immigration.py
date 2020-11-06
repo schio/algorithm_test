86da767f809357dd95638a2fbbeff4cd5f21b645
schio <ghsehr1@gmail.com>
2020-11-06 23:51:41 +0900

    Create Immigration.py

---
+++ programmers/Immigration.py
@@ -0,0 +1,17 @@
+# https://programmers.co.kr/learn/courses/30/lessons/43238
+def solution(n, times):
+    left, right = 1, max(times) * n
+    min_time = 0
+    while left <= right:
+        mid = (left + right) // 2
+        print(mid, 10 // 2)
+        th = n
+        for i in times:
+            th -= mid // i
+            if th <= 0:
+                min_time = mid
+                right = mid - 1
+                break
+        if th > 0:
+            left = mid + 1
+    return min_time
