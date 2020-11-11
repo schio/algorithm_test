4568415faf1a868f8c5767218f752a518f185466
schio <ghsehr1@gmail.com>
2020-11-11 23:56:03 +0900

    Update inner_product.py

--- programmers/inner_product.py
+++ programmers/inner_product.py
@@ -1,2 +1,3 @@
 # https://programmers.co.kr/learn/courses/30/lessons/70128def solution(a, b):
-return sum([num1 * num2 for num1, num2 in zip(a, b)])
+def solution(a, b):
+    return sum([num1 * num2 for num1, num2 in zip(a, b)])
