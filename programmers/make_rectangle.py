8e6368c863fc3eddb02d6530356b1f671af25d4e
schio <ghsehr1@gmail.com>
2020-11-15 23:27:44 +0900

    Create make_rectangle.py

---
+++ programmers/make_rectangle.py
@@ -0,0 +1,6 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12969
+a, b = map(int, input().strip().split(' '))
+for _ in range(b):
+    for _ in range(a):
+        print("*", end="")
+    print()
